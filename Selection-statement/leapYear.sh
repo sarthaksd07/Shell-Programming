50e82e04f69b201db469e233e28a15c6a8264c31
sarthaksd07 <sarthaksd07@gmail.com>
2021-06-04 15:34:14 +0530

    Refactor :Selection and case statements

--- Selection-statement/leapYear.sh
+++ Selection-statement/leapYear.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
 
-read -p "Enter a year :: " y
-if [ $y % 4 ]
+read -p "Enter a year:" y
+if [ $(($y%400)) -eq 0 ]
 then
-echo $y is a Leap Year
+echo "$y is a Leap Year"
+elif [ $(($y%100)) -eq 0 ]
+then
+echo "$y is not a Leap Year"
+elif [ $(($y%4)) -eq 0 ]
+then
+echo "$y is a Leap Year"
 else
-echo $y is Not a Leap Year
+echo "$y is Not a Leap Year"
 fi
 
--- Selection-statement/maxMin.sh
+++ Selection-statement/maxMin.sh
@@ -1,9 +1,27 @@
 #!/bin/bash -x
 
-digit1=((RANDOM%999))
-digit2=((RANDOM%999))
-digit3=((RANDOM%999))
-digit4=((RANDOM%999))
-digit5=((RANDOM%999))
+digit1=$((RANDOM%999))
+digit2=$((RANDOM%999))
+digit3=$((RANDOM%999))
+digit4=$((RANDOM%999))
+digit5=$((RANDOM%999))
 
-
+if [ $digit1 -gt $digit2 && $digit1 -gt $digit3 && $digit1 -gt $digit4 $$ $digit1 -gt $digit5 ]
+then
+echo "$Digit1 is maximum"
+fi
+elif [ $digit2 -gt $digit3 && $digit2 -gt $digit4 && $digit2 -gt $digit5 ]
+then
+echo "$Digit2 is maximum"
+fi
+elif [ $digit3 -gt $digit4 && $digit3 -gt $digit5 ]
+then
+echo "$digit3 is maximum"
+fi
+elif [ $digit4 -gt $digit5 ]
+then
+echo "$digit4 is maximum"
+fi
+else
+echo "$digit5 is maximum"
+fi
